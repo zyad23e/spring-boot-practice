9d751381fd49d555c29c987c8ac8d0340edaf015
zyad23e <zyade23@arizona.edu>
2025-09-30 12:58:36 -0700

    Edited application.properties to hard code some values and included them in the main program using @Value when specific endpoints are called

---
+++ 01-spring-boot-overview/06-properties-demo/src/main/java/restcontroller/restcontrollerR/RestcontrollerRApplication.java
@@ -0,0 +1,13 @@
+package restcontroller.restcontrollerR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestcontrollerRApplication {
+
+	public static void main(String[] args) {
+        SpringApplication.run(RestcontrollerRApplication.class, args);
+	}
+
+}

9bb3e0d523eebf2c9a0feff89871842c235eab33
zyad23e <zyade23@arizona.edu>
2025-09-30 08:42:39 -0700

    Created first local server with RestController

---
+++ restcontrollerR/src/main/java/restcontroller/restcontrollerR/controller/FunRestController.java
@@ -0,0 +1,16 @@
+package restcontroller.restcontrollerR.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class FunRestController {
+
+    //expose "/" that return "Hello World";
+
+    @GetMapping("/")
+    public String sayHello(){
+        return "Hello World";
+    }
+}

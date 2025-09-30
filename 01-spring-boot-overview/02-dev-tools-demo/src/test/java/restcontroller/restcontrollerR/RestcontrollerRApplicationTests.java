95e392258f41b36585f522a2b698cbd0041a50dd
zyad23e <zyade23@arizona.edu>
2025-09-30 08:56:24 -0700

    added exercise folder

---
+++ 01-spring-boot-overview/02-dev-tools-demo/src/test/java/restcontroller/restcontrollerR/RestcontrollerRApplicationTests.java
@@ -0,0 +1,13 @@
+package restcontroller.restcontrollerR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestcontrollerRApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

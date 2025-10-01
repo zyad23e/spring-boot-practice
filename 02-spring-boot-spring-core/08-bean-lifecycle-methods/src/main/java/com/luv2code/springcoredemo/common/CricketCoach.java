7fff8d9ba57fd23a8c78dfae8ec7d47c4ffb841e
zyad23e <zyade23@arizona.edu>
2025-10-01 16:24:28 -0700

    included @PostConstruct and @PreDestroy annotations for methods that will run once a bean is created and before a bean is destroyed.

---
+++ 02-spring-boot-spring-core/08-bean-lifecycle-methods/src/main/java/com/luv2code/springcoredemo/common/CricketCoach.java
@@ -0,0 +1,30 @@
+package com.luv2code.springcoredemo.common;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CricketCoach implements Coach {
+
+    public CricketCoach(){
+        System.out.println("In constructor:" + getClass().getSimpleName());
+    }
+
+    @Override
+    public String getDailyWorkout(){
+        return "Go on a 15 minute run!!! ;)";
+    }
+
+    // define our init method
+    @PostConstruct
+    public void doMyStartupStuff(){
+        System.out.println("In doMyStartupStuff(): " + getClass().getSimpleName());
+    }
+
+    // define our destroy method
+    @PreDestroy
+    public void doMyCleanupStuff(){
+        System.out.println("In doMyCleanupStuff(): " + getClass().getSimpleName());
+    }
+}

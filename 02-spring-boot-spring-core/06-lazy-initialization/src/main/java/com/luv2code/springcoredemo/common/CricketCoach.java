77742b75e083f53f772da963bbec1374a5ca20c8
zyad23e <zyade23@arizona.edu>
2025-10-01 15:06:46 -0700

    made use of @Lazy and globally configured lazy on all beans to only instantiate the beans we need.

---
+++ 02-spring-boot-spring-core/06-lazy-initialization/src/main/java/com/luv2code/springcoredemo/common/CricketCoach.java
@@ -0,0 +1,15 @@
+package com.luv2code.springcoredemo.common;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class CricketCoach implements Coach {
+
+    public CricketCoach(){
+        System.out.println("In constructor:" + getClass().getSimpleName());
+    }
+    @Override
+    public String getDailyWorkout(){
+        return "Go on a 15 minute run!!! ;)";
+    }
+}

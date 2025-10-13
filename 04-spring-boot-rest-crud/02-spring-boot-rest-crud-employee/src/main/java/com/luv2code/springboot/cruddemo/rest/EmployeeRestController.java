fdcbb0f67752845e90eb813cdf31a5428ee8131b
zyad23e <zyade23@arizona.edu>
2025-10-13 13:56:06 -0700

    Add a service package in order to use best practice of having a service layer than connects or communicates with a DAO layer

--- 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
+++ 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
@@ -2,6 +2,8 @@ package com.luv2code.springboot.cruddemo.rest;
 
 import com.luv2code.springboot.cruddemo.dao.EmployeeDAO;
 import com.luv2code.springboot.cruddemo.entity.Employee;
+import com.luv2code.springboot.cruddemo.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,16 +13,17 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class EmployeeRestController {
-    private EmployeeDAO employeeDAO;
+    private EmployeeService employeeService;
 
     // quick and dirty: inject employee dao
-    public EmployeeRestController(EmployeeDAO theEmployeeDAO){
-        this.employeeDAO = theEmployeeDAO;
+    @Autowired
+    public EmployeeRestController(EmployeeService theEmployeeService){
+        this.employeeService = theEmployeeService;
     }
 
     // expose "/employees" and return a list of employees
     @GetMapping("/employees")
     public List<Employee> findAll(){
-        return this.employeeDAO.findAll();
+        return this.employeeService.findAll();
     }
 }

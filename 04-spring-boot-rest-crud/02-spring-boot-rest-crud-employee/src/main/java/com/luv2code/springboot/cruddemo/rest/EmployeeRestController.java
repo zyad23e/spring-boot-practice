38ba321b01703137cb5c5d531f445230136c4eda
zyad23e <zyade23@arizona.edu>
2025-10-13 14:20:49 -0700

    Updated the Controller so it handles get to a single employee id

--- 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
+++ 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
@@ -1,10 +1,10 @@
 package com.luv2code.springboot.cruddemo.rest;
 
-import com.luv2code.springboot.cruddemo.dao.EmployeeDAO;
 import com.luv2code.springboot.cruddemo.entity.Employee;
 import com.luv2code.springboot.cruddemo.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,7 +15,6 @@ import java.util.List;
 public class EmployeeRestController {
     private EmployeeService employeeService;
 
-    // quick and dirty: inject employee dao
     @Autowired
     public EmployeeRestController(EmployeeService theEmployeeService){
         this.employeeService = theEmployeeService;
@@ -25,5 +24,17 @@ public class EmployeeRestController {
     @GetMapping("/employees")
     public List<Employee> findAll(){
         return this.employeeService.findAll();
+    }
+
+    // get a single employee by id
+    @GetMapping("/employees/{employeeId}")
+    public Employee getEmployee(@PathVariable int employeeId){
+        Employee theEmployee = employeeService.findById(employeeId);
+
+        if (theEmployee == null){
+            throw new RuntimeException("Employee id not found - " + employeeId);
+        }
+        
+        return theEmployee;
     }
 }

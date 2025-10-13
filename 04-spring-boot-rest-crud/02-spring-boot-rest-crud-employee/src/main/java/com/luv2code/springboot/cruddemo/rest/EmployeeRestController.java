e750b7971e4a29b7e93d16e0616e16b606e61f69
zyad23e <zyade23@arizona.edu>
2025-10-13 15:33:55 -0700

    Updated methods in EmployeeRestController to handle partial updates(PATCH) accurately

--- 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
+++ 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
@@ -4,17 +4,57 @@ import com.luv2code.springboot.cruddemo.entity.Employee;
 import com.luv2code.springboot.cruddemo.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import tools.jackson.databind.ObjectMapper;
+import tools.jackson.databind.node.ObjectNode;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/api")
 public class EmployeeRestController {
     private EmployeeService employeeService;
+    private ObjectMapper objectMapper;
 
     @Autowired
-    public EmployeeRestController(EmployeeService theEmployeeService){
+    public EmployeeRestController(EmployeeService theEmployeeService, ObjectMapper theObjectMapper){
         this.employeeService = theEmployeeService;
+        this.objectMapper = theObjectMapper;
+    }
+
+    // add mapping for PATCH /employees/{employeeId} - patch employee, partial update
+    @PatchMapping("/employees/{employeeId}")
+    public Employee patchEmployee(@PathVariable int employeeId, @RequestBody Map<String, Object> patchPayLoad){
+        Employee tempEmployee = employeeService.findById(employeeId);
+        
+        // throw exception if null
+        if (tempEmployee == null){
+            throw new RuntimeException("Employee id not found - " + employeeId);
+        }
+        
+        // throw exception if request body contains "id" key
+        if (patchPayLoad.containsKey("id")){
+            throw new RuntimeException("Employee id not allowed in request body - " + employeeId);
+        }
+        
+        Employee patchedEmployee = apply(patchPayLoad, tempEmployee);
+        Employee dbEmployee = employeeService.save(patchedEmployee);
+
+        return dbEmployee;
+    }
+
+    private Employee apply(Map<String, Object> patchPayLoad, Employee tempEmployee) {
+        // convert employee object to a JSON object node
+        ObjectNode employeeNode = objectMapper.convertValue(tempEmployee, ObjectNode.class);
+
+        // convert the patchpayload map to a JSON object node
+        ObjectNode patchNode = objectMapper.convertValue(patchPayLoad, ObjectNode.class);
+
+        // merge the patch updates into the employee node
+        employeeNode.setAll(patchNode);
+
+        return objectMapper.convertValue(employeeNode, Employee.class);
     }
 
     // expose "/employees" and return a list of employees

72a7087d697eca60e9735f5dcc68987fed40faad
zyad23e <zyade23@arizona.edu>
2025-12-11 12:39:53 -0700

    added a new @RequestMapping method in order to shout the names of the students!

--- 06-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/HelloWorldController.java
+++ 06-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/HelloWorldController.java
@@ -1,20 +1,40 @@
 package com.luv2code.springboot.thymeleafdemo.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HelloWorldController {
 
     // need a controller method to show initial HTML form
-    @RequestMapping("/showForm")
+    @RequestMapping ("/showForm")
     public String showForm(){
         return "helloworld-form";
     }
 
     // need a controller method to process the HTML form
-    @RequestMapping("/processForm")
+    @RequestMapping ("/processForm")
     public String processForm(){
+        return "helloworld";
+    }
+
+    // need a controller method to read form data and
+    // add data to the model
+    @RequestMapping ("/processFormVersionTwo")
+    public String letsShoutDude(HttpServletRequest request, Model theModel){
+        // read the request parameter from the HTML form
+        String theName = request.getParameter("studentName");
+
+        // convert the data to all uppercase
+        theName = theName.toUpperCase();
+
+        // create the message
+        String result = "Yo! " + theName;
+
+        // add message to the model
+        theModel.addAttribute("message", result);
         return "helloworld";
     }
 

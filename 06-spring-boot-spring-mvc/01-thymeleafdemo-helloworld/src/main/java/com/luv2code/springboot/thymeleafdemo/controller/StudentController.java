1c852792b3404788c4a343204e5031319c2ccde2
zyad23e <zyade23@arizona.edu>
2025-12-12 11:49:32 -0700

    added a dropdown list to our student form by hardcoding values in application.properties

--- 06-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/StudentController.java
+++ 06-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/StudentController.java
@@ -1,14 +1,21 @@
 package com.luv2code.springboot.thymeleafdemo.controller;
 
 import com.luv2code.springboot.thymeleafdemo.model.Student;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 public class StudentController {
+
+    // injecting countries listed from application.properties into the countries list
+    @Value("${countries}")
+    private List<String> countries;
 
     @GetMapping("/showStudentForm")
     public String showForm(Model theModel){
@@ -17,6 +24,7 @@ public class StudentController {
 
         // add student object as a model attribute
         theModel.addAttribute("student", theStudent);
+        theModel.addAttribute("countries", countries);
         return "student-form";
     }
 

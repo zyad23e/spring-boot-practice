eb6e25c09b6ccb879192ef551ab1dc8d3969bd0f
zyad23e <zyade23@arizona.edu>
2025-12-13 10:08:24 -0700

    created a customer controller class and a customer-form.html that creates a student form and checks for errors.

---
+++ 06-spring-boot-spring-mvc/02-validationdemo/src/main/java/com/luv2code/springdemo/mvc/CustomerController.java
@@ -0,0 +1,15 @@
+package com.luv2code.springdemo.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CustomerController {
+
+    @GetMapping("/")
+    public String showForm(Model theModel){
+        theModel.addAttribute("customer", new Customer());
+        return "customer-form";
+    }
+}

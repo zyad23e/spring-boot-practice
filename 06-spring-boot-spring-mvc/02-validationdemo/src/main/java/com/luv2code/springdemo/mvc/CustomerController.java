e732f0cdeda20359ce1a2526a59eafeb2787fed1
zyad23e <zyade23@arizona.edu>
2025-12-13 10:15:28 -0700

    added a @PostMapping to CustomerController class to handle validation and return the correct form depending on if there are errors.

--- 06-spring-boot-spring-mvc/02-validationdemo/src/main/java/com/luv2code/springdemo/mvc/CustomerController.java
+++ 06-spring-boot-spring-mvc/02-validationdemo/src/main/java/com/luv2code/springdemo/mvc/CustomerController.java
@@ -1,8 +1,12 @@
 package com.luv2code.springdemo.mvc;
 
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CustomerController {
@@ -11,5 +15,19 @@ public class CustomerController {
     public String showForm(Model theModel){
         theModel.addAttribute("customer", new Customer());
         return "customer-form";
+    }
+
+    @PostMapping("/processForm")
+    public String processForm(
+            @Valid @ModelAttribute("customer") Customer theCustomer,
+            BindingResult theBindingResult) {
+
+        if (theBindingResult.hasErrors()) {
+            return "customer-form";
+        } else {
+            return "customer-confirmation";
+        }
+
+
     }
 }

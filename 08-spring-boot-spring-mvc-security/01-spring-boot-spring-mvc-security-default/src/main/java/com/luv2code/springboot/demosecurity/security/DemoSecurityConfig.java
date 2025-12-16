8869e19285206a68323959d88c5187a54ad13669
zyad23e <zyade23@arizona.edu>
2025-12-16 12:11:57 -0700

    created new html form for the login page as well as a login controller

--- 08-spring-boot-spring-mvc-security/01-spring-boot-spring-mvc-security-default/src/main/java/com/luv2code/springboot/demosecurity/security/DemoSecurityConfig.java
+++ 08-spring-boot-spring-mvc-security/01-spring-boot-spring-mvc-security-default/src/main/java/com/luv2code/springboot/demosecurity/security/DemoSecurityConfig.java
@@ -2,14 +2,18 @@ package com.luv2code.springboot.demosecurity.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class DemoSecurityConfig {
 
+    // this creates an in-memory user store that spring security uses for authentication
+    // and role-based access control.
     @Bean
     public InMemoryUserDetailsManager userDetailsManager(){
         UserDetails john = User.builder().username("john").password("{noop}test123").roles("EMPLOYEE").build();
@@ -17,5 +21,14 @@ public class DemoSecurityConfig {
         UserDetails susan = User.builder().username("susan").password("{noop}test123").roles("EMPLOYEE", "MANAGER", "ADMIN").build();
 
         return new InMemoryUserDetailsManager(john, mary, susan);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.authorizeHttpRequests(configurer ->
+                configurer.anyRequest().authenticated()).formLogin(form ->
+                form.loginPage("/showMyLoginPage").loginProcessingUrl("/authenticateTheUser").permitAll());
+
+        return http.build();
     }
 }

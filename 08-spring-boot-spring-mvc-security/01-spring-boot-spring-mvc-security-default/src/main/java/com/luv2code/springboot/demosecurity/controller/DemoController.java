8c7998f6b0428d9729149949d71f34eb3652f365
zyad23e <zyade23@arizona.edu>
2025-12-18 11:36:15 -0700

    created an html form for leaders, which only certain roles can access.

--- 08-spring-boot-spring-mvc-security/01-spring-boot-spring-mvc-security-default/src/main/java/com/luv2code/springboot/demosecurity/controller/DemoController.java
+++ 08-spring-boot-spring-mvc-security/01-spring-boot-spring-mvc-security-default/src/main/java/com/luv2code/springboot/demosecurity/controller/DemoController.java
@@ -10,4 +10,10 @@ public class DemoController {
     public String showHome(){
         return "home";
     }
+
+    // add a request mapping for /leaders
+    @GetMapping("/leaders")
+    public String showLeaders(){
+        return "leaders";
+    }
 }

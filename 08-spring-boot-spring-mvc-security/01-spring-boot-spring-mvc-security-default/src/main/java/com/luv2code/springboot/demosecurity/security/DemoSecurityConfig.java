910c9f7fe717e88235032191a3d428c18a46d91f
zyad23e <zyade23@arizona.edu>
2025-12-17 17:20:35 -0700

    added logout functionality for any users logged in.

--- 08-spring-boot-spring-mvc-security/01-spring-boot-spring-mvc-security-default/src/main/java/com/luv2code/springboot/demosecurity/security/DemoSecurityConfig.java
+++ 08-spring-boot-spring-mvc-security/01-spring-boot-spring-mvc-security-default/src/main/java/com/luv2code/springboot/demosecurity/security/DemoSecurityConfig.java
@@ -27,7 +27,9 @@ public class DemoSecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http.authorizeHttpRequests(configurer ->
                 configurer.anyRequest().authenticated()).formLogin(form ->
-                form.loginPage("/showMyLoginPage").loginProcessingUrl("/authenticateTheUser").permitAll());
+                form.loginPage("/showMyLoginPage").loginProcessingUrl("/authenticateTheUser").permitAll()).logout(logout ->
+                logout.permitAll()
+        );
 
         return http.build();
     }

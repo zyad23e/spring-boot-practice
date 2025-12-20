5198f565dd7a1a8c7a89c5872cf34937bfad5499
zyad23e <zyade23@arizona.edu>
2025-12-20 10:51:43 -0700

    created new entity class to map to db table and defined the accurate fields

---
+++ 09-spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni/src/main/java/com/luv2code/cruddemo/entity/InstructorDetail.java
@@ -0,0 +1,69 @@
+package com.luv2code.cruddemo.entity;
+
+import jakarta.persistence.*;
+
+// annotate the class as an entity and map to db table
+@Entity
+@Table(name="instructor_detail")
+public class InstructorDetail {
+
+    // define the fields
+    // annotate the fields with the db column names
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private int id;
+
+    @Column(name="youtube_channel")
+    private String youtubeChannel;
+
+    @Column(name="hobby")
+    private String hobby;
+
+    // create constructors
+    public InstructorDetail(){
+
+    }
+
+    public InstructorDetail(String youtubeChannel, String hobby) {
+        this.youtubeChannel = youtubeChannel;
+        this.hobby = hobby;
+    }
+
+    // generate getter/setter methods
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getYoutubeChannel() {
+        return youtubeChannel;
+    }
+
+    public void setYoutubeChannel(String youtubeChannel) {
+        this.youtubeChannel = youtubeChannel;
+    }
+
+    public String getHobby() {
+        return hobby;
+    }
+
+    public void setHobby(String hobby) {
+        this.hobby = hobby;
+    }
+
+    // generate toString() method
+
+    @Override
+    public String toString() {
+        return "InstructorDetail{" +
+                "id=" + id +
+                ", youtubeChannel='" + youtubeChannel + '\'' +
+                ", hobby='" + hobby + '\'' +
+                '}';
+    }
+}

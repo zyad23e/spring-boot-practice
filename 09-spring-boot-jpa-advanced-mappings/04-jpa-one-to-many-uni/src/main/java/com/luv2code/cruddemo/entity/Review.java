7cfd2ac8484cc5cfe7702f40fe00c40c116690a6
zyad23e <zyade23@arizona.edu>
2025-12-24 10:33:35 -0700

    created copy of the previous project, added in a new Review entity class to map to our database, it has a OneToMany uni relationship with the Course entity

---
+++ 09-spring-boot-jpa-advanced-mappings/04-jpa-one-to-many-uni/src/main/java/com/luv2code/cruddemo/entity/Review.java
@@ -0,0 +1,60 @@
+package com.luv2code.cruddemo.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "review")
+public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "comment")
+    private String comment;
+
+    @Column(name = "course_id")
+    private Course course_id;
+
+    public Review(){
+
+    }
+
+    public Review(String comment) {
+        this.comment = comment;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public Course getCourse_id() {
+        return course_id;
+    }
+
+    public void setCourse_id(Course course_id) {
+        this.course_id = course_id;
+    }
+
+    @Override
+    public String toString() {
+        return "Review{" +
+                "id=" + id +
+                ", comment='" + comment + '\'' +
+                ", course_id=" + course_id +
+                '}';
+    }
+}

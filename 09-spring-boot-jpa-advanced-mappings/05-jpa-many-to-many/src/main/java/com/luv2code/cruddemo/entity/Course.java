d63328ad56f63faf518fbfad13901dc864b9367f
zyad23e <zyade23@arizona.edu>
2025-12-24 15:22:32 -0700

    set a @ManyToMany relationship in the Course class to join the Student and Course tables together by using JoinTable with course_student

--- 09-spring-boot-jpa-advanced-mappings/05-jpa-many-to-many/src/main/java/com/luv2code/cruddemo/entity/Course.java
+++ 09-spring-boot-jpa-advanced-mappings/05-jpa-many-to-many/src/main/java/com/luv2code/cruddemo/entity/Course.java
@@ -27,6 +27,10 @@ public class Course {
     @JoinColumn(name = "course_id")
     private List<Review> reviews;
 
+    @ManyToMany(fetch = FetchType.LAZY, cascade = {CascadeType.MERGE, CascadeType.DETACH, CascadeType.PERSIST, CascadeType.REFRESH})
+    @JoinTable(name = "course_student", joinColumns = @JoinColumn(name = "course_id"), inverseJoinColumns = @JoinColumn(name = "student_id"))
+    private List<Student> students;
+
     // define constructors
     public Course(){
 
@@ -68,6 +72,21 @@ public class Course {
 
     public void setReviews(List<Review> reviews) {
         this.reviews = reviews;
+    }
+
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(List<Student> students) {
+        this.students = students;
+    }
+
+    // add a convenience method
+    public void addStudent(Student theStudent){
+        if (students == null){
+            students = new ArrayList<>();
+        } students.add(theStudent);
     }
 
     // add a convenience method

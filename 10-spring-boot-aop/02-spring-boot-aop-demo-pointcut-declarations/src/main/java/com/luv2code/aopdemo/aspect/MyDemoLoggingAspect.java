9ee235072cc55ca79f8dbdda6c9c48417a8b13fc
zyad23e <zyade23@arizona.edu>
2025-12-30 13:42:31 -0700

    refactored our MyDemoLoggingAspect class to exclude getter/setter methods for our advice expressions

--- 10-spring-boot-aop/02-spring-boot-aop-demo-pointcut-declarations/src/main/java/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ 10-spring-boot-aop/02-spring-boot-aop-demo-pointcut-declarations/src/main/java/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -18,12 +18,24 @@ public class MyDemoLoggingAspect {
     @Pointcut("execution(* com.luv2code.aopdemo.dao.*.*(..))")
     private void forDaoPackage(){}
 
-    @Before("forDaoPackage()")
+    // create a pointcut for getter methods
+    @Pointcut("execution(* com.luv2code.aopdemo.dao.*.get*(..))")
+    private void getter(){}
+
+    // create a pointcut for setter methods
+    @Pointcut("execution(* com.luv2code.aopdemo.dao.*.set*(..))")
+    private void setter(){}
+
+    // create pointcut: include package ... exclude getter/setters
+    @Pointcut("forDaoPackage() && !(getter() || setter())")
+    private void forDaoPackageNoGetterSetter(){}
+
+    @Before("forDaoPackageNoGetterSetter()")
     public void beforeAddAccountAdvice(){
         System.out.println("\n=====>>> Executing @Before advice on method");
     }
 
-    @Before("forDaoPackage()")
+    @Before("forDaoPackageNoGetterSetter()")
     public void performApiAnalytics(){
         System.out.println("\n=====>>> Performing API analytics");
     }

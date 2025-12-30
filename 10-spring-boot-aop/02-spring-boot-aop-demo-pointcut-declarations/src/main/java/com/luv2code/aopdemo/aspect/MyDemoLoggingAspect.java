259b0d731fc4ef6b19845b1fa7ac65569fe947ed
zyad23e <zyade23@arizona.edu>
2025-12-30 14:06:57 -0700

    refactored our aspect classes ti include the @Order annotation specifying which order these classes should run

--- 10-spring-boot-aop/02-spring-boot-aop-demo-pointcut-declarations/src/main/java/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ 10-spring-boot-aop/02-spring-boot-aop-demo-pointcut-declarations/src/main/java/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -2,11 +2,13 @@ package com.luv2code.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 // java class that has collection of related advices
 @Aspect
 @Component
+@Order(2)
 public class MyDemoLoggingAspect {
 
     // this is where we add all of our related advices for logging
@@ -14,7 +16,7 @@ public class MyDemoLoggingAspect {
     // let's start with a @Before advice
     // pointcut expression, run this code BEFORE - target object method:
 
-    @Before("forDaoPackageNoGetterSetter()")
+    @Before("com.luv2code.aopdemo.aspect.LuvAopExpressions.forDaoPackageNoGetterSetter()")
     public void beforeAddAccountAdvice(){
         System.out.println("\n=====>>> Executing @Before advice on method");
     }

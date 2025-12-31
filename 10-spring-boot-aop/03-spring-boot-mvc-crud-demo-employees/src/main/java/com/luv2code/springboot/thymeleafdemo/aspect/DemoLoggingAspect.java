9f3c63a665486ba48d727d2d6feff79ab1d0aa0f
zyad23e <zyade23@arizona.edu>
2025-12-31 12:54:32 -0700

    updated our DemoLoggingAspect class with a @AfterReturning advice to display the method we are returning from as well as the data returned

--- 10-spring-boot-aop/03-spring-boot-mvc-crud-demo-employees/src/main/java/com/luv2code/springboot/thymeleafdemo/aspect/DemoLoggingAspect.java
+++ 10-spring-boot-aop/03-spring-boot-mvc-crud-demo-employees/src/main/java/com/luv2code/springboot/thymeleafdemo/aspect/DemoLoggingAspect.java
@@ -1,6 +1,7 @@
 package com.luv2code.springboot.thymeleafdemo.aspect;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -45,5 +46,18 @@ public class DemoLoggingAspect {
         for (Object o: args){
             myLogger.info("=====>> argument: " + o);
         }
+    }
+
+    // add @AfterReturning advice
+    @AfterReturning(pointcut = "forAppFlow()", returning = "theResult")
+    public void afterReturning(JoinPoint theJoinPoint, Object theResult){
+
+        // display the method we are returning from
+        String theMethod = theJoinPoint.getSignature().toShortString();
+        myLogger.info("=====>> in @AfterReturning: from method: " + theMethod);
+
+        // display the data returned
+        myLogger.info("=====>> result: " + theResult);
+
     }
 }

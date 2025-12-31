3bce107633ffab4c9c9432176fbec90a5ab4d532
zyad23e <zyade23@arizona.edu>
2025-12-31 11:54:56 -0700

    added a new service package and 2 classes to simulate a traffic service

---
+++ 10-spring-boot-aop/02-spring-boot-aop-demo-pointcut-declarations/src/main/java/com/luv2code/aopdemo/service/TrafficFortuneService.java
@@ -0,0 +1,6 @@
+package com.luv2code.aopdemo.service;
+
+public interface TrafficFortuneService {
+
+    String getFortune();
+}
---
+++ 10-spring-boot-aop/02-spring-boot-aop-demo-pointcut-declarations/src/main/java/com/luv2code/aopdemo/service/TrafficFortuneServiceImpl.java
@@ -0,0 +1,25 @@
+package com.luv2code.aopdemo.service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
+
+@Service
+public class TrafficFortuneServiceImpl implements TrafficFortuneService{
+
+    @Override
+    public String getFortune() {
+
+        // simulate a delay
+
+        // this line of code will delay the application by 5 seconds
+        try {
+            TimeUnit.SECONDS.sleep(5);
+        } catch (InterruptedException e){
+            throw new RuntimeException(e);
+        }
+
+        // return a fortune
+        return "Expect heavy traffic this morning";
+    }
+}

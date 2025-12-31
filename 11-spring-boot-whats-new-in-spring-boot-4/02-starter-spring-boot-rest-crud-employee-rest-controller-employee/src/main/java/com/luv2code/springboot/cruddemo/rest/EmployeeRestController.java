64ed4afec91f135453e91d124f762f463e60c20c
zyad23e <zyade23@arizona.edu>
2025-12-31 13:12:42 -0700

    added previous project

---
+++ 11-spring-boot-whats-new-in-spring-boot-4/02-starter-spring-boot-rest-crud-employee-rest-controller-employee/src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
@@ -0,0 +1,130 @@
+package com.luv2code.springboot.cruddemo.rest;
+
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.luv2code.springboot.cruddemo.entity.Employee;
+import com.luv2code.springboot.cruddemo.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api")
+public class EmployeeRestController {
+
+    private EmployeeService employeeService;
+
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    public EmployeeRestController(EmployeeService theEmployeeService, ObjectMapper theObjectMapper) {
+        employeeService = theEmployeeService;
+        objectMapper = theObjectMapper;
+    }
+
+    // expose "/employees" and return a list of employees
+    @GetMapping("/employees")
+    public List<Employee> findAll() {
+        return employeeService.findAll();
+    }
+
+    // add mapping for GET /employees/{employeeId}
+
+    @GetMapping("/employees/{employeeId}")
+    public Employee getEmployee(@PathVariable int employeeId) {
+
+        Employee theEmployee = employeeService.findById(employeeId);
+
+        if (theEmployee == null) {
+            throw new RuntimeException("Employee id not found - " + employeeId);
+        }
+
+        return theEmployee;
+    }
+
+    // add mapping for POST /employees - add new employee
+
+    @PostMapping("/employees")
+    public Employee addEmployee(@RequestBody Employee theEmployee) {
+
+        // also just in case they pass an id in JSON ... set id to 0
+        // this is to force a save of new item ... instead of update
+
+        theEmployee.setId(0);
+
+        Employee dbEmployee = employeeService.save(theEmployee);
+
+        return dbEmployee;
+    }
+
+    // add mapping for PUT /employees - update existing employee
+
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee theEmployee) {
+
+        Employee dbEmployee = employeeService.save(theEmployee);
+
+        return dbEmployee;
+    }
+
+    // add mapping for PATCH /employees/{employeeId} - patch employee ... partial update
+
+    @PatchMapping("/employees/{employeeId}")
+    public Employee patchEmployee(@PathVariable int employeeId,
+                                  @RequestBody Map<String, Object> patchPayload) throws JsonMappingException {
+
+        Employee tempEmployee = employeeService.findById(employeeId);
+
+        // throw exception if null
+        if (tempEmployee == null) {
+            throw new RuntimeException("Employee id not found - " + employeeId);
+        }
+
+        // throw exception if request body contains "id" key
+        if (patchPayload.containsKey("id")) {
+            throw new RuntimeException("Employee id not allowed in request body - " + employeeId);
+        }
+
+        // Apply the partial updates to the existing employee object.
+        Employee patchedEmployee = objectMapper.updateValue(tempEmployee, patchPayload);
+
+        Employee dbEmployee = employeeService.save(patchedEmployee);
+
+        return dbEmployee;
+    }
+
+    // add mapping for DELETE /employees/{employeeId} - delete employee
+
+    @DeleteMapping("/employees/{employeeId}")
+    public String deleteEmployee(@PathVariable int employeeId) {
+
+        Employee tempEmployee = employeeService.findById(employeeId);
+
+        // throw exception if null
+
+        if (tempEmployee == null) {
+            throw new RuntimeException("Employee id not found - " + employeeId);
+        }
+
+        employeeService.deleteById(employeeId);
+
+        return "Deleted employee id - " + employeeId;
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+

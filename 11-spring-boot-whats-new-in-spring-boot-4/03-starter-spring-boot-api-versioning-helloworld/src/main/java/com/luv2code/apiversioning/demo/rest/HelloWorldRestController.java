a8b17703f38d65db81b77d6f24f3ca7107221da5
zyad23e <zyade23@arizona.edu>
2025-12-31 13:25:58 -0700

    updated controller class for api versioning as well as our application.properties file

--- 11-spring-boot-whats-new-in-spring-boot-4/03-starter-spring-boot-api-versioning-helloworld/src/main/java/com/luv2code/apiversioning/demo/rest/HelloWorldRestController.java
+++ 11-spring-boot-whats-new-in-spring-boot-4/03-starter-spring-boot-api-versioning-helloworld/src/main/java/com/luv2code/apiversioning/demo/rest/HelloWorldRestController.java
@@ -1,14 +1,26 @@
 package com.luv2code.apiversioning.demo.rest;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/{version}/hello")
 public class HelloWorldRestController {
 
-    @GetMapping(path="/api/hello")
+    @GetMapping(version = "1")
     public String helloV1() {
-        return "Hello World";
+        return "Hello World from API v1";
+    }
+
+    @GetMapping(version = "2")
+    public String helloV2() {
+        return "Howdy World from API v2";
+    }
+
+    @GetMapping(version = "3")
+    public String helloV3() {
+        return "Hey Hey World from API v3";
     }
 
 }
